a8976f8e18982f7b5f2bc7e4a4905f03276b168f
CarlosCMB1525 <info@communitybolivia.com>
2026-01-29 13:43:47 -0400

    Delete modules directory

--- modules/cashflow/src/Admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace CMBERP\Modules\Cashflow;
-if (!defined('ABSPATH')) exit;
-final class Admin {
-  public static function register(): void {
-    // (opcional) settings por admin_post, si lo necesitas más adelante
-  }
-}
